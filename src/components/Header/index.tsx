4252fa7fa9c596a2890ef80ce28c3e4c7c453553
Matheus Lúcio <matheusluciox@gmail.com>
2023-05-20 15:37:19 -0300

    rework do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,44 +8,39 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="bg-white text-white p-4">
+    <header className="bg-white text-white p-5">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center">
           {/* Logo */}
-          <div className="mr-4">
+          <div className="mr-8">
             <Link href="/" className="text-xl font-bold">
-              <img src="/assets/imgs/logo.png" alt="Logo"></img>
+              <img src="/assets/imgs/logo.png" alt="Logo" />
             </Link>
           </div>
           {/* Campo de pesquisa */}
-          <div className="max-w-96 relative">
+          <div className="max-w-96 relative hidden lg:flex">
             <span className="absolute left-3 top-1/2 transform -translate-y-1/2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-gray-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817-1.414 1.414-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <img className="h-5 w-5 text-gray-400" src="/assets/svg/search.svg" alt="Search" />
             </span>
             <input
               type="text"
               placeholder="Pesquisar eventos, shows, cursos..."
-              className="w-full pl-10 pr-3 py-2 bg-white text-black rounded"
+              className="w-full pl-10 p-4 bg-white text-black rounded-xl border-gray-200 focus:border-purple-tf-900 outline-none border lg:min-w-[380px] text-lg"
             />
           </div>
         </div>
         {/* Menu */}
         <div className="hidden md:flex items-center">
-          <Link href="/produtor" className="mr-4 text-gray-400">
-            Seja um produtor
+          <Link
+            href="/produtor"
+            className="mr-4 text-lg text-gray-tf-400 hover:text-gray-400"
+          >
+            Seja um produtor >
           </Link>
-          <Link href="/get-started" className="mr-4 text-gray-400">
+          <Link
+            href="/get-started"
+            className="mr-4 text-lg text-gray-tf-400 hover:text-gray-400"
+          >
             Get Started
           </Link>
           {/* Botão de login */}
